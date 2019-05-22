e75f2a0c0b991029838bf2ca2735b4806acf1cb9
Jesus Bibieca <jesusbibieca52@gmail.com>
2019-05-21 20:38:59 -0400

    Initial commit :tada:

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hi everyone!</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque eligendi maiores libero id rem aliquid deleniti perspiciatis quidem consectetur dignissimos!
+    </p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
