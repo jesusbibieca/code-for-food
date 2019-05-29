1b3e7c15fcccb1839d8bce9550cde32a876c9de9
Jesus Bibieca <jesusbibieca52@gmail.com>
2019-05-29 16:19:33 -0400

    reverting changes on img component

--- src/components/image.js
+++ src/components/image.js
@@ -13,22 +13,20 @@ import Img from 'gatsby-image';
  * - `StaticQuery`: https://gatsby.dev/staticquery
  */
 
-const Image = ({ data }) => {
-  console.log(data);
-  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />;
-};
-
-export const query = graphql`
-  query {
-    placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
-      childImageSharp {
-        # Specify the image processing specifications right in the query.
-        # Makes it trivial to update as your page's design changes.
-        fluid(maxWidth: 125) {
-          ...GatsbyImageSharpFluid
+const Image = () => (
+  <StaticQuery
+    query={graphql`
+      query {
+        placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+          childImageSharp {
+            fluid(maxWidth: 300) {
+              ...GatsbyImageSharpFluid
+            }
+          }
         }
       }
-    }
-  }
-`;
+    `}
+    render={data => <Img fluid={data.placeholderImage.childImageSharp.fluid} />}
+  />
+);
 export default Image;
