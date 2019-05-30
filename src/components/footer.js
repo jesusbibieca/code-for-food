ce6887bb067fbdb46107ba9f3838f0609a9800e4
Jesus Bibieca <jesusbibieca52@gmail.com>
2019-05-30 19:30:46 -0400

    Improving footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const Footer = () => (
-  <footer className="footer has-text-white">
+  <footer className="footer has-text-white has-text-centered">
     <div className="content">
       © {new Date().getFullYear()}, Built with
       <a href="https://www.gatsbyjs.org"> Gatsby</a>
+      <br />
+      Crafted with ❤️by <em>Jesus Bibieca</em> from the DR 🇩🇴
     </div>
   </footer>
 );
