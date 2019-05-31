9f1a717d8863343c24897965213fb9d5999c272f
Jesus Bibieca <jesusbibieca52@gmail.com>
2019-05-30 21:38:36 -0400

    adding social link components

--- src/utils/config.js
+++ src/utils/config.js
@@ -9,4 +9,26 @@ module.exports = {
       slug: '/page-2',
     },
   ],
+  userLinks: [
+    {
+      label: 'GitHub',
+      url: 'https://github.com/jesusbibieca',
+    },
+    {
+      label: 'LinkedIn',
+      url: 'https://linkedin.com/in/jesus-bibieca-450165139/',
+    },
+    {
+      label: 'Twitter',
+      url: 'https://twitter.com/jesusbibieca',
+    },
+    {
+      label: 'Email',
+      url: 'mailto:jesusbibieca52@gmail.com',
+    },
+    {
+      label: 'Instagram',
+      url: 'https://www.instagram.com/jesusbibieca/',
+    },
+  ],
 };
